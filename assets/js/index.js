55d10172d5f6a74288ec806f13614a95132a50d6
金宝 <1144837984@qq.com>
2022-09-03 13:41:13 +0800

    修改定位

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,11 @@
-$(function() {
+$(function () {
     //调用用户基本信息函数
     getUserInfo()
 
     //退出登陆监听事件
-    $('#btnLogout').on('click', function() {
+    $('#btnLogout').on('click', function () {
         //提示用户是否退出登陆
-        layui.layer.confirm('确定退出登陆', { icon: 3, title: '提示' }, function(index) {
+        layui.layer.confirm('确定退出登陆', { icon: 3, title: '提示' }, function (index) {
             //清空用户token
             localStorage.removeItem('token')
 
@@ -13,27 +13,31 @@ $(function() {
             location.href = '/login.html'
 
             //关闭弹出层
-            layer.close(index);
-        });
+            layer.close(index)
+        })
     })
-
-
 })
+
+function gitname() {
+    $.ajax({
+        method: 'POST',
+    })
+}
 
 // 获取用户基本信息函数
 function getUserInfo() {
     $.ajax({
-        type: "GET",
+        type: 'GET',
         url: '/my/userinfo',
-        success: function(res) {
+        success: function (res) {
             // console.log(res)
             if (res.status !== 0) {
-                return layui.layer.msg('获取用户信息失败');
+                return layui.layer.msg('获取用户信息失败')
             }
 
             //调用renderAvatar用户头像函数
             renderAvatar(res.data)
-        }
+        },
     })
 }
 
@@ -41,7 +45,7 @@ function getUserInfo() {
 function renderAvatar(user) {
     //渲染用户名
     var name = user.nickname || user.username
-    $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
     //渲染头像
     if (user.user_pic !== null) {
         $('.layui-nav-img').prop('src', user.user_pic).show()
